44aef84b590982a01e30c0ae70ec9b0ce11ab568
jmarcusgill <jmarcusgill@gmail.com>
2017-05-10 15:13:24 -0500

    printing array of foods to dom on click of button

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log("is this working?");
+var app = angular.module("ZoeFavFood", []);
+
+app.controller("FavFoods", ($scope) => {
+  $scope.dog = "I'mma dog";
+  $scope.foods = ["La Croix", "Chocolat Chip Cookies", "Dippin Dots", "Coffee"];
+
+  $scope.showListView = true;
+
+
+
+
+
+
+  $scope.showFood = () => {
+      $scope.showListView = false;
+      console.log("in showFood", $scope.foods);
+    };
+
+
+
+});
+
