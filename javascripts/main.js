4678c78612eaad3116a002025ad305915d2465e6
jmarcusgill <jmarcusgill@gmail.com>
2017-05-11 17:21:26 -0500

    im happy with this

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ var app = angular.module("ZoeFavFood", []);
 
 app.controller("FavFoods", ($scope) => {
   $scope.dog = "I'mma dog";
-  $scope.foods = ["Tangerine", "Lemon", "Lime", "Passionfruit", "Coconut", "Apricot", "Pear", "all of them..."];
+  $scope.foods = ["Tangerine", "Lemon", "Lime", "Passionfruit", "Coconut", "Apricot", "Peach-Pear", "Pamplemouse", "all of them..."];
 
   $scope.showListView = true;
 
